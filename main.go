87a2df2bba311f089fe8d22972214e90b3df2fc2
haikelfazzani <haikel.fazzani@hotmail.com>
2023-03-07 23:35:47 +0100

    1.0.0

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 
 func main() {
 	ntpc := &ntpc.NTPC{
-		Server: "time.google.com",
+		Server: "pool.ntp.org",
 		Port:   "123",
 	}
 
 	timeNow, _ := ntpc.Query()
 	fmt.Println("\n> NTP time:", timeNow) // 2023-03-07 21:01:53.084929939 +0000 UTC
 
-	// dateTime := timeNow.Format("2006-01-02 15:04:05")
+	dateTime := timeNow.Format("2006-01-02 15:04:05")
 
-	// isUpdated := ntpc.UpdateSystem(dateTime)
-	// fmt.Println("\n> System time updated:", isUpdated)
+	isUpdated := ntpc.UpdateSystem(dateTime)
+	fmt.Println("\n> System time updated:", isUpdated)
 }
